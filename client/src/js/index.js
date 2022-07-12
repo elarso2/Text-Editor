059128740e0cf65c6161c6530086bff07c7ba131
Elizabeth Larson <elarso2@uw.edu>
2022-07-12 16:34:23 -0700

    index, src-sw, install, and webpack.config

---
+++ client/src/js/index.js
@@ -0,0 +1,9 @@
+import { Workbox } from "workbox-window";
+import "../css/style.css";
+
+if ("serviceWorker" in navigator) {
+  const workboxSW = new Workbox("/src-sw.js");
+  workboxSW.register();
+} else {
+  console.error("Service workers are not supported in this browser.");
+}
