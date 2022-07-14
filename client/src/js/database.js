602eeefc94d9cf41d230a88a1c57d39dee8c5a40
Elizabeth Larson <elarso2@uw.edu>
2022-07-13 22:18:51 -0700

    updating my repo with the starter code I just realized we were provided :')

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,18 +2,22 @@ import { openDB } from "idb";
 
 // Function to start database
 const initdb = async () =>
-  openDB("text", 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("text")) {
-        console.log("Text database already exists");
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore("text", { keyPath: "id", autoIncrement: true });
-      console.log("Text database created");
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // Posting to database
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error("putDb not implemented");
+
 export const postDb = async (text) => {
   console.log("Posting to database");
   const textDb = await openDB("text", 1);
@@ -24,8 +28,11 @@ export const postDb = async (text) => {
   console.log("Your text has been saved to the database!", result);
 };
 
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error("getDb not implemented");
+
 // Getting saved text data from database
-export const getDb = async () => {
+export const getDatabase = async () => {
   console.log("Getting text from database");
   const textDb = await openDB("text", 1);
   const tx = contactDb.transation("text", "readonly");
