2d6f28f607ad51c04468363b88aa4e20c80f87c8
Elizabeth Larson <elarso2@uw.edu>
2022-07-14 13:04:58 -0700

    fixing spelling errors in server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("../client/dist"));
-app.use(express.urelencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
-  console.long(`Now listening on port: ${PORT}`);
+  console.log(`Now listening on port: ${PORT}`);
 });
