9941f2ad625257fa3f6e508d9606631f0d1920f2
Elizabeth Larson <elarso2@uw.edu>
2022-07-14 12:52:59 -0700

    removing db.js comments and deleteDb fxn

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,10 +14,6 @@ const initdb = async () =>
   });
 
 // Posting to database
-
-// TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => console.error("putDb not implemented");
-
 export const putDb = async (content) => {
   console.log("Posting to database");
   const jateDb = await openDB("jate", 1);
@@ -27,9 +23,6 @@ export const putDb = async (content) => {
   const result = await request;
   console.log("Your text has been saved to the database!", result);
 };
-
-// TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error("getDb not implemented");
 
 // Getting saved text data from database
 export const getDb = async () => {
@@ -41,19 +34,6 @@ export const getDb = async () => {
   const result = await request;
   console.log("result.value", result);
   return result;
-};
-
-// dont need?
-// Deleting data from database
-export const deleteDb = async (id) => {
-  console.log("Deleting text from database", id);
-  const textDb = await openDB("text", 1);
-  const tx = textDb.transaction("text", "readwrite");
-  const store = tx.objectStore("text");
-  const request = store.delete(id);
-  const result = await request;
-  console.log("result.value", result);
-  return result.value;
 };
 
 // Start database
